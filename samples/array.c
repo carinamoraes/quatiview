d2176dcb17d0f237cf08c07cb07d9912ad8bb787
Carina <carinah_moraes@hotmail.com>
2026-03-11 06:09:50 -0300

    removing comments from array code example

--- samples/array.c
+++ samples/array.c
@@ -1,43 +1,45 @@
 // Exemplo de inicialização de array com quicksort
 
-// Escolhe o último elemento como pivô
-int particionar(int vetor[], int inicio, int fim) {
+int particionar(int vetor[], int inicio, int fim)
+{
     int pivo;
-	pivo = vetor[fim];
+    pivo = vetor[fim];
     // Função para destacar um elemento de um array
-	highlight_index(vetor, fim, "#87a832");
+    highlight_index(vetor, fim, "#87a832");
     int i, j, temp;
-	i = inicio - 1;
+    i = inicio - 1;
 
-    for (j = inicio; j < fim; j = j+1) {
-        if (vetor[j] <= pivo) {
-            i = i+1;
-			temp = vetor[i];
-			vetor[i] = vetor[j];
-			vetor[j] = temp;
+    for (j = inicio; j < fim; j = j + 1)
+    {
+        if (vetor[j] <= pivo)
+        {
+            i = i + 1;
+            temp = vetor[i];
+            vetor[i] = vetor[j];
+            vetor[j] = temp;
         }
     }
 
-    // Coloca o pivô na posição correta
-	temp = vetor[i + 1];
-	vetor[i + 1] = vetor[fim];
-	vetor[fim] = temp;
+    temp = vetor[i + 1];
+    vetor[i + 1] = vetor[fim];
+    vetor[fim] = temp;
+
     // Função para limpar o destaque de um elemento de um array
-	clear_highlight(vetor);
+    clear_highlight(vetor);
+
     return i + 1;
 }
 
-// Função principal do Quick Sort
-void quickSort(int vetor[], int inicio, int fim) {
-    if (inicio < fim) {
-		int indicePivo;
-		indicePivo = particionar(vetor, inicio, fim);
-        // Ordena os elementos antes e depois do pivô
+void quickSort(int vetor[], int inicio, int fim)
+{
+    if (inicio < fim)
+    {
+        int indicePivo;
+        indicePivo = particionar(vetor, inicio, fim);
         quickSort(vetor, inicio, indicePivo - 1);
         quickSort(vetor, indicePivo + 1, fim);
     }
 }
-
 
 int main()
 {
@@ -45,8 +47,8 @@ int main()
 
     v = {45, 12, 78, 3, 56, 89, 23, 67, 1, 90,
          34, 76, 5, 88, 19, 42, 7, 99, 15, 60};
-	
-	quickSort(v, 0, 19);
+
+    quickSort(v, 0, 19);
 
     return 0;
 }
